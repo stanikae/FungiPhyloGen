7ba59ff4016d9b5051c1c4233df11e2f3b65bdbb
Stanford Kwenda <stankwenda@gmail.com>
2026-01-01 14:57:45 +0200

    Remove unused modules and conda envs

--- scripts/launch_fpg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-
-sampleList=/scratch/sysuser/stanford/git-repos/FungiPhyloGen/sample_file.csv
-refseq=~/test-data/ref-genomes/ncbi_dataset/data/GCA_000150115.1/GCA_000150115.1_ASM15011v1_genomic.fna
-gbk=~/test-data/ref-genomes/ncbi_dataset/data/GCA_000150115.1/genomic.gbff
-confg=/scratch/sysuser/stanford/git-repos/FungiPhyloGen/fpg_test.config
-nextflow run /scratch/sysuser/stanford/git-repos/FungiPhyloGen/main_test.nf --refseq $refseq --gbk $gbk --prjName "TEST-FASTQC" -c $confg -with-conda true
